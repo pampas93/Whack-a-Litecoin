9ac87cea2e1cd21273b09c8870374940a7168ac8
pampas93 <abhijit.93@hotmail.com>
2018-01-17 13:49:07 -0500

    Terrain texturing + Mobile VR Mode

---
+++ LTC Miner Android/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        VRSettings.enabled = false;
+        Camera.main.ResetAspect();
+        Camera.main.GetComponent<Transform>().localRotation = InputTracking.GetLocalRotation(VRNode.CenterEye);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Camera.main.GetComponent<Transform>().localRotation = InputTracking.GetLocalRotation(VRNode.CenterEye);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Whack-a-Litecoin
+# Whack a Litecoin
+
+A game on Litecoin, mimicking the whack a mole concept. A cross platform game, to spread the word of litecoin and its use cases.
