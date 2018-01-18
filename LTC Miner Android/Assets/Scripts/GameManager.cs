ff19cc75a3a503d09134e014f92ff3642b370976
pampas93 <abhijit.93@hotmail.com>
2018-01-18 14:32:10 -0500

    Game time - Progress bar

--- LTC Miner Android/Assets/Scripts/GameManager.cs
+++ LTC Miner Android/Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.VR;
+using ProgressBar;
+using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
+
+    public ProgressBarBehaviour barScript;
+    public ProgressRadialBehaviour radialScript;
+
+    private IEnumerator coroutine;
+
+    public bool timeUp = false;
 
     private void Awake()
     {
@@ -20,11 +27,33 @@ public class GameManager : MonoBehaviour {
         Camera.main.ResetAspect();
         Camera.main.GetComponent<Transform>().localRotation = InputTracking.GetLocalRotation(VRNode.CenterEye);
 
+        barScript.IncrementValue(100);
+        radialScript.IncrementValue(100);
+
+        coroutine = timeCoroutine();
+        StartCoroutine(coroutine);
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         Camera.main.GetComponent<Transform>().localRotation = InputTracking.GetLocalRotation(VRNode.CenterEye);
+    }
+
+    IEnumerator timeCoroutine()
+    {
+        while (!timeUp)
+        {
+            if (barScript.Value == 0)
+                timeUp = true;
+
+            barScript.DecrementValue(2);
+            radialScript.DecrementValue(2);
+            yield return new WaitForSeconds(1);
+        }
+
+        Debug.Log("TimeUp");
+        
     }
 }
--- LTC Miner Android/Assets/Scripts/Shoot.cs
+++ LTC Miner Android/Assets/Scripts/Shoot.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.EventSystems;
+
 
 public class Shoot : MonoBehaviour
 {
@@ -8,31 +7,33 @@ public class Shoot : MonoBehaviour
     public GameObject bullet;
     public float bulletSpeed;
     public Transform bulletSpawn;
-    public TextMesh txtDebug;
-    public TextMesh txt2;
+    
+
 
     void Update()
     {
-        //Web
-        if (Input.GetMouseButtonDown(0))
+        if (!GameManager.instance.timeUp)
         {
+            ////Web
+            //if (Input.GetMouseButtonDown(0))
+            //{
 
-            var bt = (GameObject)Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-            bt.GetComponent<Rigidbody>().velocity = bt.transform.forward * bulletSpeed;
+            //    var bt = (GameObject)Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+            //    bt.GetComponent<Rigidbody>().velocity = bt.transform.forward * bulletSpeed;
+            //    Destroy(bt, 4.0f);
+            //}
 
-            Destroy(bt, 4.0f);
+            //Android
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    var bt = (GameObject)Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+                    bt.GetComponent<Rigidbody>().velocity = bt.transform.forward * bulletSpeed;
+
+                    Destroy(bt, 4.0f);
+                }
+            }
         }
-
-        ////Android
-        //if (Input.touchCount > 0)
-        //{
-        //    if (Input.GetTouch(0).phase == TouchPhase.Began)
-        //    {
-        //        var bt = (GameObject)Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-        //        bt.GetComponent<Rigidbody>().velocity = bt.transform.forward * bulletSpeed;
-
-        //        Destroy(bt, 4.0f);
-        //    }
-        //}
     }
 }
