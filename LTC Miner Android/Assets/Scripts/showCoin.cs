a6cf46d816520c3e2c30936875422512ec4995d0
pampas93 <abhijit.93@hotmail.com>
2018-01-19 22:05:22 -0500

    Litecoin FBX and Animations import

---
+++ LTC Miner Android/Assets/Scripts/showCoin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showCoin : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        //anim.SetTrigger("OnClick");
+    }
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log("Clicked");
+        //    anim.SetTrigger("OnClick");
+        //}
+
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Debug.Log("right Clicked");
+        //    anim.SetTrigger("OnRightClick");
+        //}
+
+        //if (Input.GetKeyDown("space"))
+        //    anim.speed = 0;
+    }
+
+    public void makeActive()
+    {
+        anim.SetTrigger("OnClick");
+    }
+
+}
