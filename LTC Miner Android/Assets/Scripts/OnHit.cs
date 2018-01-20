8952e056e54f9733e2bc898d84a7e05987247a45
pampas93 <abhijit.93@hotmail.com>
2018-01-20 04:27:26 -0500

    LTC pots and animations

---
+++ LTC Miner Android/Assets/Scripts/OnHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHit : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Hit an object!");
+        //Debug.Log(collision.collider.gameObject.name);
+        this.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
